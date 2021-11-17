b1ca5a4352fb2a0775ef6fb6ca7ce4d4c1ef0a59
bfbrug <bfbrug@gmail.com>
2021-11-17 17:25:43 -0400

    Upload de avatar e files

--- src/middlewares/ensureAutheticated.ts
+++ src/middlewares/ensureAutheticated.ts
@@ -36,6 +36,10 @@ export async function ensureAutheticated(
       throw new AppError('User does not exist!', 401);
     }
 
+    request.user = {
+      id: user_id,
+    };
+
     next();
   } catch {
     throw new AppError('Invalid token!', 401);
